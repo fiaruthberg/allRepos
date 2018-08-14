d4d7704abc84f0d72fbe1553b8be4f44872632dd
AWA5CG8126F2G\Administrator <sofia.ruthberg@gmail.com>
2018-08-14 11:30:39 +0200

    Böcker 1

---
+++ Böcker 1/Böcker 1/Book.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Böcker_1
+{
+    class Book
+    {
+        private string author { get; set; }
+        private string isbn { get; set; }
+        private int nrOfPages { get; set; }
+
+
+        internal void SetIsbn(string newIsbn)
+        {
+            isbn = newIsbn;
+
+        }
+
+        internal object GetAuthor()
+        {
+            return author;
+        }
+
+        internal object GetNrOfPages()
+        {
+            if (nrOfPages > 1000)
+            {
+                nrOfPages = 300;
+            }
+            return nrOfPages;
+        }
+
+        internal object Size()
+        {
+            string size;
+
+            if (nrOfPages < 100)
+            {
+                size = "Tunn";
+                return size;
+            }
+            else if (nrOfPages > 300)
+            {
+                size = "Tjock";
+                return size;
+            }
+            else
+            {
+                size = "Normal";
+                return size;
+            }
+        }
+
+        internal object WeightInGram()
+        {
+            double grams = nrOfPages * 0.8;
+            return grams;
+        }
+
+        internal object GetIsbn()
+        {
+            return isbn;
+        }
+
+        internal void SetNrOfPages(int newNrOfPages)
+        {
+            nrOfPages = newNrOfPages;
+        }
+
+        internal void SetAuthor(string newAuthor)
+        {
+            author = newAuthor;
+        }
+
+
+
+    }
+}
---
+++ Böcker 1/Böcker 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Böcker_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = new Book();
+
+            b1.SetIsbn("978-3-16-148410-0");
+            b1.SetAuthor("Kalle");
+            b1.SetNrOfPages(400);
+
+            Console.WriteLine($"Info om boken:");
+            Console.WriteLine();
+            Console.WriteLine($"   ISBN:        {b1.GetIsbn()}");
+            Console.WriteLine($"   Författare:  {b1.GetAuthor()}");
+            Console.WriteLine($"   Antal sidor: {b1.GetNrOfPages()}");
+            Console.WriteLine($"   Vikt:        {b1.WeightInGram()} gram");
+            Console.WriteLine($"   Storlek:     {b1.Size()}");
+            Console.WriteLine();
+        }
+    }
+}
