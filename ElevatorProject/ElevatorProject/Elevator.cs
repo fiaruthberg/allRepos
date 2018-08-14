1deba14c45b99f546945179056aa5ac9f6b9c45c
AWA5CG8126F2G\Administrator <sofia.ruthberg@gmail.com>
2018-08-14 18:54:44 +0200

    ElevatorProject

---
+++ ElevatorProject/ElevatorProject/Elevator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorProject
+{
+    class Elevator
+    {
+        public string name { get; set; }
+        public int lowestFloor { get; set; }
+        public int highestFloor { get; set; }
+        public int currentFloor { get; set; }
+        public int timeUntilMaintenance { get; set; }
+        public string onOrOff
+        {
+            get
+            {
+                if (timeUntilMaintenance > 0)
+                    return "på";
+
+                    return "av";
+            }
+        }
+
+        public Elevator(string a, int b, int c, int d, int e)
+        {
+            name = a;
+            lowestFloor = b;
+            highestFloor = c;
+            currentFloor = d;
+            timeUntilMaintenance = e;
+        }
+        public Elevator(string a, int b, int c)
+        {
+            name = a;
+            lowestFloor = b;
+            highestFloor = c;
+            currentFloor = 5;
+            timeUntilMaintenance = 10;
+        }
+
+        internal void GoUp()
+        {
+            if (timeUntilMaintenance > 0)
+            {
+                if (currentFloor < highestFloor)
+                {
+                    currentFloor++;
+                    timeUntilMaintenance--;
+                }
+            }
+        }
+
+
+        internal string Report()
+        {
+            return $"Hissen {name} är på våning {currentFloor}. Hissen är {onOrOff}. Tills underhåll: {timeUntilMaintenance}";
+        }
+
+        internal void GoDown()
+        {
+            if (timeUntilMaintenance > 0)
+            {
+                if (currentFloor > lowestFloor)
+                {
+                    currentFloor--;
+                    timeUntilMaintenance--;
+                }
+            }
+            
+        }
+
+    }
+}
---
+++ ElevatorProject/ElevatorProject/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test av Kalle
+
+            Header("SKAPAR HISSEN KALLE");
+            var e1 = new Elevator("Kalle", -2, 10, 5, 2);
+            Console.WriteLine("     " + e1.Report());
+            Info("Signal till Kalle att åka upp en våning");
+            e1.GoUp();
+            Console.WriteLine("     " + e1.Report());
+
+            Info("Signal till Kalle att åka upp en våning");
+            e1.GoUp();
+            Console.WriteLine("     " + e1.Report());
+
+            Info("Signal till Kalle att åka upp en våning");
+            e1.GoUp();
+            Console.WriteLine("     " + e1.Report());
+
+            // Test av Lisa (hissen är redan på lägsta nivå så kommer inte åka nånstans)
+
+            Header("SKAPAR HISSEN LISA");
+            var e2 = new Elevator("Lisa", 5, 8);
+            Console.WriteLine("     " + e2.Report());
+            Info("Signal till Lisa att åka ner en våning");
+            e2.GoDown();
+            Console.WriteLine("     " + e2.Report());
+            Console.WriteLine();
+        }
+
+        private static void Info(string v)
+        {
+            
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("     " + v);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void Header(string v)
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(v);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        }
+    }
+}
