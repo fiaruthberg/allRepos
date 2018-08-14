4a0e4773c89641d7107984ed06f9f8cea4eea093
AWA5CG8126F2G\Administrator <sofia.ruthberg@gmail.com>
2018-08-14 11:30:21 +0200

    Upg 7.1

---
+++ Ny Console App 2/7.1 Setup classes/7.1 Setup classes/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1_Setup_classes
+{
+    
+    
+    class Shape
+    {
+        public double height { get; set; }
+        public double width { get; set; }
+    }
+ 
+    
+   
+}
---
+++ Ny Console App 2/7.1 Setup classes/7.1 Setup classes/Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1_Setup_classes
+{
+    class Triangle : Shape
+    {
+        public Triangle(double x, double y)
+        {
+            height = x;
+            width = y;
+        }
+        
+    }
+}
