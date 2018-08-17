e7f52b307c68e1e630f941f0993143a84e860a1f
AWA5CG8126F2G\Administrator <sofia.ruthberg@gmail.com>
2018-08-17 14:33:27 +0200

    11.2 Work with list of Costumers

---
+++ 11.2 Work with list of Costumers/11.2 Work with list of Costumers/Costumer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._2_Work_with_list_of_Costumers
+{
+    class Costumer
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+
+    }
+
+    class Parser
+    {
+        internal List<Costumer> CreateListOfCostumers(string v)
+        {
+            List<Costumer> list = new List<Costumer>();
+            var allLines = File.ReadAllLines(v);
+            
+            foreach (var line in allLines.Skip(1))
+            {
+                var splittedLine = line.Split(',');
+                var id = splittedLine[0];
+                var firstName = splittedLine[1];
+                var lastName = splittedLine[2];
+                var email = splittedLine[3];
+                var gender = splittedLine[4];
+                var age = splittedLine[5];
+                
+                var costumer = new Costumer();
+                costumer.Id = id;
+                costumer.FirstName = firstName;
+                costumer.LastName = lastName;
+                costumer.Email = email;
+                costumer.Gender = gender;
+                costumer.Age = int.Parse(age);
+
+
+                list.Add(costumer);
+
+
+            }
+                return list;
+        }
+    }
+}
---
+++ 11.2 Work with list of Costumers/11.2 Work with list of Costumers/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._2_Work_with_list_of_Costumers
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            List<Costumer> list = parser.CreateListOfCostumers(@"C:\Project\AcceleratedLearning\C#\PURE\11.2 Work with list of Costumers\11.2 Work with list of Costumers\PersonShort.csv");
+            Header("Sorted list by age:");
+            PrintListByAge(list);
+            Header("Sorted list by first name:");
+            PrintListByFirstName(list);
+            Header("Men older then 35:");
+            OlderMenThan35(list);
+            Header("Manipulated:");
+            Manipulated(list);
+
+
+        }
+
+        private static void Manipulated(List<Costumer> list)
+        {
+            var xxx = list.Where(y => y.Age > 35).OrderBy(y => y.FirstName);
+
+            foreach (var info in xxx)
+            {
+                Console.WriteLine($"{info.FirstName.ToUpper().PadRight(20)}{(info.Age+1000).ToString().PadRight(20)}{info.Gender}");
+            }
+
+        }
+
+        private static void OlderMenThan35(List<Costumer> list)
+        {
+            var xxx = list.Where(y => y.Age > 35).OrderBy(y => y.Age);
+
+            foreach (var info in xxx)
+            {
+                WriteInfoAboutShow(info);
+            }
+
+        }
+
+        private static void PrintListByAge(List<Costumer> list)
+        {
+            var xxx = list.OrderBy(y => y.Age);
+
+            foreach (var info in xxx)
+            {
+                WriteInfoAboutShow(info);
+            }
+
+        }
+
+        private static void PrintListByFirstName(List<Costumer> list)
+        {
+            var xxx = list.OrderBy(y => y.FirstName);
+
+            foreach (var info in xxx)
+            {
+                WriteInfoAboutShow(info);
+            }
+
+        }
+
+        private static void WriteInfoAboutShow(Costumer info)
+        {
+            Console.WriteLine($"{info.FirstName.PadRight(20)}{info.Age.ToString().PadRight(20)}{info.Gender}");
+        }
+        private static void Header(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"\n{s}");
+            Console.ResetColor();
+        }
+    }
+}
