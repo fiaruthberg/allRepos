b501c8e23d779e94529a5370f8c2e0bd5ed318e4
AWA5CG8126F2G\Administrator <sofia.ruthberg@gmail.com>
2018-08-17 14:30:25 +0200

    TV Tablå (Codealong)

---
+++ TV Tablå/TV Tablå/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_Tablå
+{
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var allLines = File.ReadAllLines("tv-data.txt");
+
+            var allShows = new List<Show>();
+
+            foreach (var line in allLines)
+            {
+                var splittedLine = line.Split('*');
+                var channel = splittedLine[0];
+                var time = splittedLine[1];
+                var title = splittedLine[2];
+
+                var show = new Show();
+                show.Title = title;
+                show.Channel = channel;
+                show.StartAt = TimeSpan.Parse(time);
+
+                allShows.Add(show);
+            }
+
+            Header("alla titlar");
+
+            foreach (var show in allShows)
+            {
+                Console.WriteLine(show.Title);
+            }
+
+            Console.WriteLine();
+
+            Header("alla kanaler");
+
+            foreach (var show in allShows)
+            {
+                Console.WriteLine(show.Channel);
+            }
+
+            Header("alla titlar");
+
+            var laterThan21 = new List<Show>();
+
+            foreach (var show in allShows)
+            {
+                if (show.StartAt.Hours >= 21)
+                {
+                    laterThan21.Add(show);
+                }
+            }
+
+            var x = allShows.Where(y => y.StartAt.Hours >= 21);
+
+            foreach (var show in x)
+            {
+                WriteInfoAboutShow(show);
+            }
+
+            // Annat sätt att sortera
+            // allShows.Sort((a,b) => string.CompareOrdinal(a.Title, b.Title));
+
+            Console.WriteLine();
+
+            Header("program från svt1 i kronologisk ordning");
+
+            var xxx = allShows.Where(y => y.Channel == "SVT1").OrderBy(y => y.StartAt);
+            foreach (var show in xxx)
+            {
+                WriteInfoAboutShow(show);
+            }
+
+
+            Console.WriteLine();
+
+        }
+
+        private static void Header(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n{s.ToUpper()}\n");
+            Console.ResetColor();
+        }
+
+        private static void WriteInfoAboutShow(Show show)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"{show.Channel.PadRight(4)} {show.StartAt} {show.Title}");
+        }
+    }
+}
