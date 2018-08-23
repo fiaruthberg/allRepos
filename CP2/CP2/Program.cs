100406ef4962c735119df6d1885e814b12ec1982
AWA5CG8126F2G\Administrator <sofia.ruthberg@gmail.com>
2018-08-23 08:49:04 +0200

    Checkpoint 2

---
+++ CP2/CP2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("-----------------------------------------------------");
+                Console.WriteLine();
+                string input = GetInputFromUser();
+                if (!ValidateInput(input))
+                {
+                    continue;
+                }
+                List<Room> roomList = CreateListOfRooms(input);
+                Console.WriteLine();
+                LightsOn(roomList);
+                LargestRoom(roomList);
+                TotalRooms(roomList);
+                Console.WriteLine();
+            }
+        }
+
+        private static bool ValidateInput(string input)
+        {
+            var allRooms = new List<Room>();
+
+            var splittedLine = input.Split('|');
+
+            foreach (string newLine in splittedLine)
+            {
+                if (!Regex.IsMatch(newLine, @"^\s*[a-zåäö]{3,15}\s\d+(m2)\s(off|on)\s*$", RegexOptions.IgnoreCase))
+                {
+                    WriteInRed($"\nOgiltlig indata\n");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void TotalRooms(List<Room> roomList)
+        {
+            var xxx = roomList;
+
+            WriteInGreen($"Lägenheten består av {xxx.Count()} rum.");
+        }
+        private static void LargestRoom(List<Room> roomList)
+        {
+            var xxx = roomList.OrderByDescending(y => y.Size);
+
+            foreach (var info in xxx)
+            {
+
+                WriteInGreen($"Det största rummet är {info.Name}, på {info.Size}");
+                break;
+            }
+        }
+        private static void LightsOn(List<Room> roomList)
+        {
+            var xxx = roomList.Where(y => y.LightsOnOff.ToUpper() == "ON").OrderBy(y => y.Name);
+
+            if (xxx.Count() <= 0)
+            {
+                WriteInGreen("Inget rum är tänt");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"Ljuset är tänt i ");
+                foreach (var info in xxx)
+                {
+                    Console.Write($"{info.Name} ");
+
+                }
+            }
+
+        }
+        private static void WriteInGreen(string v)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(v);
+            Console.ResetColor();
+        }
+        private static List<Room> CreateListOfRooms(string input)
+        {
+            var allRooms = new List<Room>();
+
+            var splittedLine = input.Split('|');
+
+            foreach (string newLine in splittedLine)
+            {
+                if (!Regex.IsMatch(newLine, @"^\s*[a-zåäö]{3,15}\s\d+(m2)\s(off|on)\s*$", RegexOptions.IgnoreCase))
+                {
+                    WriteInRed($"Ogiltlig indata");
+                    break;
+
+                }
+                char separator = ' ';
+                var doubleSplittedLine = newLine.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (doubleSplittedLine?.Length < 2)
+                {
+                    continue;
+                }
+                var nameOfRoom = doubleSplittedLine[0];
+                var size = doubleSplittedLine[1];
+                var lightsOnOff = doubleSplittedLine[2];
+
+                var room = new Room();
+                room.Name = nameOfRoom;
+                room.Size = size;
+                room.LightsOnOff = lightsOnOff;
+
+                allRooms.Add(room);
+
+            }
+            return allRooms;
+        }
+        private static void WriteInRed(string v)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(v);
+            Console.ResetColor();
+        }
+        private static string GetInputFromUser()
+        {
+            Header("Ange rum i lägenheten: ");
+            string input = Console.ReadLine();
+
+            return input;
+        }
+        private static void Header(string v)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(v);
+            Console.ResetColor();
+        }
+    }
+}
